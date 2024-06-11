f30c10aee759b02d3d71ef324e0e08e1c3829db3
meg546 <meg546@nau.edu>
2024-06-10 20:44:07 -0700

    Adding Comments to code

--- Homework1/index.js
+++ Homework1/index.js
@@ -1,17 +1,20 @@
 
+
+/* First section get a random dice image within the specific range  */
+/* get random number */
 var randomNumber = Math.floor(Math.random() * 6) + 1;
-
+/* create image name using random number */
 var diceImage = "dice" + randomNumber + ".png";
-
+/* assign dice image to the correct pathway */
 var diceImageSource = "images/" + diceImage;
 
-
+/* Query the document for the image tags, change the image src to the newly create image source */
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", diceImageSource);
 
 
 
-
+/* repeat the process again for the second image */
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 var diceImage2 = "dice" + randomNumber2 + ".png";
@@ -22,9 +25,10 @@ var image2 = document.querySelectorAll("img")[1];
 image2.setAttribute("src", diceImageSource2);
 
 
+/* Check which number is bigger than the other, that number wins */
 if (randomNumber > randomNumber2)
     {
-
+    /* change the inner HTML text to the player that has won */
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
         
     }
@@ -35,7 +39,7 @@ else if (randomNumber < randomNumber2)
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
 
     }
-
+/* account for draws  */
 else 
     {
         document.querySelector("h1").innerHTML = "Draw!";
